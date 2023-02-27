682c9e0cbe184e64e9849b783d17c024af8d3212
Alex Som <alex.som86@gmail.com>
2023-02-27 10:09:26 +0600

    add orders component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import { Layout } from './components/Layout';
 import Checkout from './containers/Checkout/Checkout';
 import ContactData from './containers/Checkout/ContactData/ContactData';
+import { Orders } from './containers/Orders';
 import PizzaBuilder from './containers/PizzaBuilder/PizzaBuilder';
 
 function App() {
@@ -13,6 +14,8 @@ function App() {
         <Route path="/checkout" element={<Checkout />}>
           <Route path="contact-data" element={<ContactData />} />
         </Route>
+        <Route path="/orders" element={<Orders />} />
+        <Route path="*" element={<h1>404 Не найдена страница</h1>} />
       </Routes>
     </Layout>
   );
